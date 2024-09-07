aa5405ec69f465e31dd211c2fcccd02d3fed17cb
mikekondo <foad5969@gmail.com>
2024-09-07 16:48:02 +0900

    立替リストのviewをいい感じにした

--- CoupleWallet/Feature/PayList/PayListScreenView.swift
+++ CoupleWallet/Feature/PayList/PayListScreenView.swift
@@ -4,16 +4,18 @@ struct PayListScreenView<VM: PayListViewModel>: View {
     @StateObject var vm: VM
     var body: some View {
         ScrollView {
-            LazyVStack(alignment: .leading, spacing: 8) {
+            LazyVStack(alignment: .leading, spacing: 16) {
                 ForEach(vm.payViewDataList) { viewData in
                     Button {
                         vm.didTapPayCell(id: viewData.id)
                     } label: {
                         payCell(viewData: viewData)
                     }
-                    Divider()
+                    .background(Color.white)
+                    .cornerRadius(12)
+                    .shadow(color: Color.gray.opacity(0.2), radius: 5, x: 0, y: 2)
+                    .padding(.horizontal, 16)
                 }
-                .padding(.horizontal, 16)
             }
         }
         .refreshable {
@@ -35,18 +37,20 @@ extension PayListScreenView {
         HStack(spacing: 0) {
             VStack(alignment: .leading, spacing: 8) {
                 Text(viewData.title)
-                    .font(.callout)
+                    .font(.title3.weight(.bold))
                     .foregroundStyle(Color.black)
                 Text(viewData.byName)
                     .font(.callout)
                     .foregroundStyle(Color.gray)
                 Text(viewData.dateText)
                     .font(.caption2)
+                    .foregroundStyle(Color.gray.opacity(0.7))
             }
             .frame(maxWidth: .infinity, alignment: .leading)
+
             HStack(spacing: 16) {
                 Text(viewData.priceText)
-                    .font(.title3)
+                    .font(.title2.weight(.semibold))
                     .foregroundStyle(Color.black)
                 Menu {
                     Button {
@@ -54,18 +58,21 @@ extension PayListScreenView {
                             await vm.didTapDeleteButton(id: viewData.id)
                         }
                     } label: {
-                        Text("削除")
+                        Label("削除", systemImage: "trash")
                     }
                 } label: {
                     Image(systemName: "ellipsis")
                         .foregroundStyle(Color.gray)
+                        .padding()
                 }
+                .background(Color.white)
+                .cornerRadius(8)
             }
         }
+        .padding()
     }
 }
 
 #Preview {
     PayListScreenView(vm: PayListViewModelImpl())
 }
-
